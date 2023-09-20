8af34fdb11454466390cecf04697998ea1f2797a
David Kroll <dkroll713@gmail.com>
2023-09-20 14:06:24 -0500

    added event listeners so it continues working

--- content.js
+++ content.js
@@ -1,22 +1,33 @@
-// const clownCheck = () => {
-let comments = document.getElementsByClassName("author");
-console.log('Comments:', comments)
-for (let i = 0; i < comments.length; i++) {
-  let comment = comments[i];
-  console.log('Comment:', comment.innerText)
-  let flair = comment.previousElementSibling;
-  if (flair && flair.classList.contains("flair")) {
-    console.log('User has flair');
-  } else {
-    console.log('Lol no flair');
-    let element = document.createElement("span");
-    element.textContent = '🤡'
-    element.style = 'font-size: 1.5em; margin-left: 0.5em; margin-right 0.5em;'
-    comment.parentNode.insertBefore(element, comment);
+const clownCheck = () => {
+  let comments = document.getElementsByClassName("author");
+  for (let i = 0; i < comments.length; i++) {
+    let comment = comments[i];
+    let flair = comment.previousElementSibling;
+    if (!flair || flair.classList.contains("flair") || flair.classList.contains("customflair")) {
+      // continue;
+    } else {
+      let element = document.createElement("span");
+      element.textContent = '🤡'
+      element.classList.add("customflair");
+      element.style = 'font-size: 1.5em; margin-left: 0.5em; margin-right 0.5em;'
+      comment.parentNode.insertBefore(element, comment);
+    }
   }
 }
-// }
 
-// document.addEventListener('DOMContentLoaded', () => clownCheck());
 
-// 🤡
+const addClownCheckToEventListeners = () => {
+  let buttons = document.getElementsByClassName("button");
+  for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.addEventListener("click", () => {
+      setTimeout(() => {
+        clownCheck();
+      }, 250)
+    });
+  }
+}
+
+
+clownCheck();
+addClownCheckToEventListeners();
